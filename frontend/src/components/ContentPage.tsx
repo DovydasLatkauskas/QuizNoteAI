c41d4c42c5b2e039a2d5c95dcba76e76fa074cb4
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2025-01-25 15:45:45 -0500

    content page fully flushed out

--- frontend/src/components/ContentPage.tsx
+++ frontend/src/components/ContentPage.tsx
@@ -1,12 +1,19 @@
 "use client"
- 
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  IconPlus,
+} from "@tabler/icons-react"; 
+import { CirclePlus } from "lucide-react"
+
+
+// Form controlling
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm, Controller } from "react-hook-form"
 
-import React, { useState } from "react";
+// UI Components
 import { Button } from "./ui/button"
-import { motion, AnimatePresence } from "framer-motion";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -25,13 +32,42 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 
-// THINGS WE WILL NEED FROM BACKEND
+
+
+// TODO: Backend:
 // - Get the users groups
 // - Get the files in each group
 // - Get the content of each file
 // - CRUD operations on Groups
+
+// TODO: Frontend:
+// - Select all button for each group
+
 
 export function SelectGroupMenu({ control } : { control: any }) {
   return (
@@ -55,10 +91,10 @@ export function SelectGroupMenu({ control } : { control: any }) {
   )
 }
 
-
-
 export default function ContentPage(){
   const [checkedItems, setCheckedItems] = useState<string[]>([]);
+  const [activeTab, setActiveTab] = useState<string>("All"); 
+  const [contentModalOpen, setContentModalOpen] = useState<boolean>(true);
 
   let groups = [
     {
@@ -70,26 +106,38 @@ export default function ContentPage(){
       ]
     },
     {
-      title: "CISC 454",
+      title: "CISC 372",
       files: [
         {filepath: "lecture_01.mp3", type: "audio", content: "0:00 - some other description example"},
         {filepath: "lecture_02.mp4", type: "video", content: "0:00 - some other description example"},
-        {filepath: "lecture_03.mp4", type: "video", content: "0:00 - some other description example"},
+      ]
+    },
+    {
+      title: "CISC 235",
+      files: [
+        {filepath: "lecture_01.pdf", type: "audio", content: "0:00 - some other description example"},
+        {filepath: "content_lecture.pdf", type: "document", content: "0:00 - some other description example"},
       ]
     },
   ]
 
-  // Content component with a checkbox
+  const handleCheckboxChange = (filePath: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setCheckedItems((prev) => [...prev, filePath]);
+    } else {
+      setCheckedItems((prev) => prev.filter((item) => item !== filePath));
+    }
+  };
+
   interface ContentProps {
-    title: string;
+    index: number;
+    filePath: string;
+    content: string;
     type: string;
     checked: boolean;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   }
 
-
-
-  // UPLOAD CONTENT FORM
   const formSchema = z.object({
     file: z
       .any()
@@ -97,7 +145,6 @@ export default function ContentPage(){
       .transform((file) => file?.[0]), // Take the first file from FileList
     selectGroup: z.string().nonempty("Group is required"),
   });
-  
 
   function InputContentForm() {
     const form = useForm<z.infer<typeof formSchema>>({
@@ -170,38 +217,116 @@ export default function ContentPage(){
   );
 }
 
+function UploadContentDialog({ contentModalOpen, setContentModalOpen }: { 
+  contentModalOpen: boolean,
+  setContentModalOpen: React.Dispatch<React.SetStateAction<boolean>> 
+}) {
+  return (
+    <Dialog >
+      <DialogTrigger asChild>
+        <Button onClick={() => {
+          setContentModalOpen(true);
+        }} className="w-fit bg-blue-500 text-white">
+          <CirclePlus/> Add Content
+        </Button>
+      </DialogTrigger>
 
-  const Content: React.FC<ContentProps> = ({ title, type, checked, onChange }) => {
-    return (
-      <AnimatePresence>
-        <motion.div
-          key={title}
-          exit={{ opacity: 0 }}
-          className="gap-2 rounded-md aspect-square w-44 mb-2 bg-[#e5e7eb]"
-          animate={{ backgroundColor: checked ? "rgb(191, 219, 254)" : "rgb(229, 231, 235)" }} // bg-blue-200 and bg-gray-200
-          transition={{ duration: 0.3 }}
-        >
-          <div className="flex flex-row items-center gap-2 p-2">
-            <input 
-              className="peer h-5 w-5 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground" 
-              type="checkbox" 
-              checked={checked} 
+      <DialogContent className="max-w-fit">
+        <DialogHeader >
+          <DialogTitle>Add Content</DialogTitle>
+          <DialogDescription>
+            Upload PDF's, Video lectures, Audio files and more.
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <InputContentForm></InputContentForm>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+const Content: React.FC<ContentProps> = ({ index, filePath, content, type, checked, onChange }) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        whileHover={{
+          scale: 1.05, // Slightly increase size on hover
+          cursor: "pointer", // Change cursor to pointer
+        }}
+        animate={{
+          scale: checked ? 1.03 : 1, // Increase size if checked
+        }}
+        transition={{
+          duration: 0.2, // Animation duration
+          ease: "easeInOut", // Smooth easing
+        }}
+      >
+        <Card key={index} className={`w-72 ${checked ? "bg-blue-100" : ""}`}>
+          <CardHeader className="flex flex-row items-center gap-2">
+            <input
+              type="checkbox"
+              checked={checked}
               onChange={onChange}
+              className="h-4 w-4 rounded"
             />
-            <p className="font-sans text-lg">{title}</p>
-          </div>
-        </motion.div>
-      </AnimatePresence>
+            <CardTitle>{filePath}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>{content.substring(0, 30)}...</CardDescription>
+          </CardContent>
+        </Card>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+  function GroupTabsMenu({ groups } : { groups: any }) {
+
+    let allFiles = groups.map((group: any) => group.files).flat();
+    groups.unshift({title: "All", files: allFiles});
+    let numOfCols = "grid-cols-" + groups.length;
+    const defaultGroup = groups.length > 0 ? groups[0].title : "";
+    
+    return(
+      <Tabs defaultValue={defaultGroup} className="w-full" value={activeTab} onValueChange={setActiveTab}>
+        <TabsList className={`grid w-full ${numOfCols} gap-4`}>
+          {groups.map((group: any, index: number) => (
+            <TabsTrigger key={index} value={group.title}>{group.title}</TabsTrigger>
+          ))}
+        </TabsList>
+  
+        {groups.map((group: any, index: number) => (
+          <TabsContent key={index} value={group.title}>
+            <Card className="">
+              <CardHeader>
+                <CardTitle>{group.title}</CardTitle>
+                <CardDescription>
+                  All your files associated with {group.title}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-row gap-4 flex-wrap">
+                {group.files.map((file: any, index: number) => (
+                  <Content
+                    key={index}
+                    index={index}
+                    filePath={file.filepath}
+                    content={file.content}
+                    type={file.type}
+                    checked={checkedItems.includes(file.filepath)}
+                    onChange={handleCheckboxChange(file.filepath)}
+                  />
+                ))}
+              </CardContent>
+              <CardFooter>
+                {/* <Button className="bg-red-500" onClick={() => {}}>Delete Group</Button> */}
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        ))}
+      </Tabs>
     )
   }
-
-  const handleCheckboxChange = (title: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      setCheckedItems((prev) => [...prev, title]);
-    } else {
-      setCheckedItems((prev) => prev.filter((item) => item !== title));
-    }
-  };
 
   return(
     <div className="flex flex-1">
@@ -213,29 +338,13 @@ export default function ContentPage(){
 
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 
       bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full overflow-y-auto">
-        <h2 className="text-2xl font-semibold dark:text-white">Content</h2>
-        {/* <Button onClick={() => {}} className="w-24">Add Content</Button> */}
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <InputContentForm />
-        </div>
-        
-        <h3 className="text-2xl mt-12">Files</h3>
-        <div className="flex flex-row gap-4 w-full flex-wrap">
-          {groups.map((group, index) => (
-            <div key={index}>
-              <h3 className="text-xl">{group.title}</h3>
-              {group.files.map((file, index) => (
-                <Content
-                  key={index}
-                  title={file.filepath}
-                  type={file.type}
-                  checked={checkedItems.includes(file.filepath)}
-                  onChange={handleCheckboxChange(file.filepath)}
-                />
-              ))}
-            </div>
-          ))}
-        </div>
+        <h2 className="text-3xl font-semibold dark:text-white">Content</h2>
+        { contentModalOpen ? 
+          <UploadContentDialog contentModalOpen={contentModalOpen} setContentModalOpen={setContentModalOpen}/>
+          : 
+          null
+        }
+        <GroupTabsMenu groups={groups} />
      </div>
    </div>
   )
